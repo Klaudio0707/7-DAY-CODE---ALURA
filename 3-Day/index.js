e736f9af440efc17e8ac638a6401762ebe264706
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-07-02 17:24:47 -0300

    4º dia

---
+++ 3-Day/index.js
@@ -0,0 +1,18 @@
+
+const resp = prompt('Você quer seguir com a área\n Front-End ou Back-End');
+const linguagem = [];
+
+if (resp == "Front-End") {
+  alert("parabéns! no futuro você será um desenvolvedor " + resp);
+  const linguagens= prompt("Suas Linguagens?");
+
+ for (let index = 0; index < linguagem.length; index++) {
+ const  = linguagem[index];
+    
+ }
+  
+linguagem.push(linguagens);
+
+alert(`${linguagem}\n`);
+
+}
